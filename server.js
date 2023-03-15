894480fe251b44dde72c8c164e54d76610530c50
neordovah <92874574+neordovah@users.noreply.github.com>
2023-03-15 17:39:04 +0200

    Update server.js

--- server.js
+++ server.js
@@ -194,7 +194,7 @@ if(process.env.NODE_ENV === "production") {
 const start = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI)
-        app.listen(3001, () => {
+        app.listen(process.env.PORT || 3001, () => {
             console.log("Sever is listening on port 3001...")
         })
     }
